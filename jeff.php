8247752518933affcd519bd5c50c3b164e5f2485
telusmasjeffson-ai <telusmasjeffson@gmail.com>
2026-02-19 17:39:14 -0500

    update

--- jeff.php
+++
@@ -1,6 +0,0 @@
-$otp = rand(100000,999999);
-$to = $email;
-$subject = "Verification Code";
-$message = "Your OTP code is: " . $otp;
-$headers = "From: noreply@yoursite.com";
-mail($to, $subject, $message, $headers);
